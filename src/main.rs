0fb1e1aa6f2bbe22fd2ac73c5238d087eb279936
Luiz Guilherme Costa da Silva <luiz.developer.acc@gmail.com>
2022-05-26 17:52:46 -0300

    finished, but slow as hell

--- src/main.rs
+++ src/main.rs
@@ -21,16 +21,19 @@ fn main() {
     let dic_to_load = String::from(&args[2]);
     let dictionary: Vec<String> = load_dictionary(dic_to_load).unwrap();
     
-//    for (i, item) in words.iter().enumerate() {
-//        println!("{} - {}", i, format!("{}", item).bold().blue());
-//    }
-    let indeces_stts = words_stts(&dictionary, &words);
-    /*for (index, word) in words.iter().enumerate() {
-        match indeces_stts[index] {
-            1 => println!("{}", format!("{}", word).bold().blue()),
-            _ => println!("{}", format!("{}", word).bold().red()),
+    let mut indeces_stts = Vec::new();
+    words_stts(&dictionary, &words, &mut indeces_stts);
+    
+    for (i, status) in indeces_stts.iter().enumerate() {
+        if *status == 1 {
+            print!("{} ", format!("{}", words[i]).green());
+        } else {
+            print!("{} ", format!("{}", words[i]).red());
         }
-    }*/
+        
+    } 
+    println!();
+    
 }
 
 fn get_words (s: &str, vec: &mut Vec<String>) {
@@ -40,7 +43,7 @@ fn get_words (s: &str, vec: &mut Vec<String>) {
 
     for (i, &item) in text.iter().enumerate() {
         match item {
-            b' ' | b'\n' | b',' | b'.' | b';' | b':' | b'"' => {                
+            b' ' | b'\n' | b',' | b'.' | b';' | b':' | b'"' | b'!' | b'?' | b'-' => {                
                 vec.push(s[prev_slice..i].to_string().to_lowercase());
                 prev_slice = i + 1;
             } 
@@ -68,32 +71,13 @@ fn load_dictionary (dic: String) -> Option<Vec<String>> {
     }
 }
 
-fn words_stts (dic_words: &Vec<String>, vec_words: &Vec<String>) -> Vec<u8>{
-    let mut word_stts: Vec<u8> = Vec::new();
-    let mut init: usize = 0;
+fn words_stts (dic_words: &Vec<String>, vec_words: &Vec<String>, vec_status: &mut Vec<i32>){
     
-    for (indx, text_word) in vec_words.iter().enumerate() {
-        let text_bytes = text_word.as_bytes();
-        let text_first_char = text_bytes[0];
-         
-        for (i, dic_word) in dic_words.iter().enumerate() {
-            let dic_bytes = dic_word.as_bytes();
-            let dic_first_char = dic_bytes[0];
-            if dic_first_char == text_first_char {
-                init = i;
-                break;
-            }
-        }
-
-        let vec_dic = &dic_words[init..];
-        for word_in_dic in vec_dic.iter() {
-            if word_in_dic == text_word {
-                word_stts.push(1);
-                break;
-            }  
-        } 
-        init = 0;
-
+    for word in vec_words.iter() {
+        let returned_from_find = dic_words.iter().find(|&string| string == word);
+        match returned_from_find {
+            Option::Some(_result) => vec_status.push(1),
+            Option::None => vec_status.push(0),
+        };
     }
-    return word_stts;
 }
